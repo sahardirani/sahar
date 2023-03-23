95ec767a30304191c9884e8b6dc5646d7cb686e3
sahardirani <125820130+sahardirani@users.noreply.github.com>
2023-03-23 11:37:19 +0200

    Create MMpthreads.c

---
+++ MMpthreads.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define M 3
+#define N 3
+#define K 3
+
+int A[M][K] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+int B[K][N] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+int C[M][N];
+
+int num_threads = 2;
+
+void* matrix_multiplication(void* arg) {
+    int thread_id = *(int*) arg;
+    int start = (thread_id * M) / num_threads;
+    int end = ((thread_id + 1) * M) / num_threads;
+
+    for (int i = start; i < end; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < K; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    pthread_t threads[num_threads];
+    int thread_id[num_threads];
+
+    for (int i = 0; i < num_threads; i++) {
+        thread_id[i] = i;
+        pthread_create(&threads[i], NULL, matrix_multiplication, &thread_id[i]);
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
