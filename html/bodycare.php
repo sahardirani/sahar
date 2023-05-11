85f6f1c90a5de97d2bca975eea67111ce4aa5c3d
sahardirani <125820130+sahardirani@users.noreply.github.com>
2023-05-11 15:04:11 +0300

    bodycare.php

---
+++ html/bodycare.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/mystylesheet.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        <title>Body Care</title>
+    </head>
+    <body>  
+        <?php require_once("common/menu.php");?>
+        
+    </body>
+</html>
